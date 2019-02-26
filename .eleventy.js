9b85b54fa53f05e26646205aa6754f1ea7aa49b6
leandrw <contato@leandrw.com.br>
2019-02-26 02:59:00 -0300

    feat: post list

--- .eleventy.js
+++ .eleventy.js
@@ -1,38 +1,74 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const { DateTime } = require('luxon');
 
 module.exports = function(eleventyConfig) {
-  /** Plugins */
-  definePlugins(eleventyConfig);
-  defineAliases(eleventyConfig);
-  defineI18nEnConfig(eleventyConfig);
-  defineStylesPath(eleventyConfig);
-  defineImagesPath(eleventyConfig);
-  defineDocs(eleventyConfig);
+  eleventy(eleventyConfig)
+    .define(
+      plugins,
+      aliases,
+      i18nEn,
+      stylesPath,
+      imagesPath,
+      docs,
+      filters,
+      collections,
+    );
 };
 
-function definePlugins(eleventyConfig) {
+function eleventy(config) {
+  return {
+    define(...parts) {
+      parts.forEach((part) => {
+        part(config);
+      });
+    }
+  };
+}
+
+function plugins(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
 }
 
-function defineAliases(eleventyConfig) {
+function aliases(eleventyConfig) {
   eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
   eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
 }
 
-function defineI18nEnConfig(eleventyConfig) {
+function i18nEn(eleventyConfig) {
   eleventyConfig.addLayoutAlias('post-en', 'layouts/post-en.njk');
   eleventyConfig.addLayoutAlias('page-en', 'layouts/page-en.njk');
 }
 
-function defineStylesPath(eleventyConfig) {
+function stylesPath(eleventyConfig) {
    eleventyConfig.addPassthroughCopy('_assets/css');
 }
 
-function defineImagesPath(eleventyConfig) {
+function imagesPath(eleventyConfig) {
    eleventyConfig.addPassthroughCopy('wp-content');
 }
 
-function defineDocs(eleventyConfig) {
+function docs(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('README.md');
 }
+
+function collections(eleventyConfig) {
+  eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'));
+}
+
+function filters(eleventyConfig) {
+  eleventyConfig.addFilter('head', filterHead.bind(this));
+  eleventyConfig.addFilter('htmlDateString', (dateObj) =>
+    DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd'));
+  eleventyConfig.addFilter('readableDate', (dateObj) =>
+    DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy"));
+
+  function filterHead(array, n) {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  }
+}
+
--- index.md
+++
@@ -1,6 +0,0 @@
----
-layout: base
----
-# Leandro Cavalcante (@leandrw)
-
-Olá, sou um desenvolvedor frontend experiente e com vontade de melhorar as coisas.
